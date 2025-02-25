c33d88663bc5f8d3641750f0aa7907b73dc9788b
Yuliia Ozkan <yulka.oak18@gmail.com>
2025-02-25 12:00:05 +0200

    App:Moved CategorySelector into home route

--- week2/project/ecommerce/src/App.jsx
+++ week2/project/ecommerce/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import CategorySelector from './components/CategorySelector';
 import ProductList from './components/ProductList';
@@ -9,37 +9,28 @@ import Spinner from './components/Spinner';
 function App() {
   const [activeCategory, setActiveCategory] = useState('');
   const { categories, products, loading, error } = useFetchData(activeCategory);
-  const location = useLocation();
-
   return (
     <div>
-      {location.pathname === '/' && (
-        <>
-          <h1>Products</h1>
-          <CategorySelector
-            categories={categories}
-            activeCategory={activeCategory}
-            onSelectCategory={setActiveCategory}
-          />
-        </>
-      )}
-
       {error && <div style={{ color: 'red' }}>{error}</div>}
-      {loading && <div className="spinner"></div>}
-
+      {loading && <Spinner />}
       <Routes>
         <Route
           path="/"
           element={
-            <div className="app-header">
-              <ProductList products={products} />
-            </div>
+            <>
+              <h1>Products</h1>
+              <CategorySelector
+                categories={categories}
+                activeCategory={activeCategory}
+                onSelectCategory={setActiveCategory}
+              />
+              <div className="app-header">
+                <ProductList products={products} />
+              </div>
+            </>
           }
         />
-        <Route
-          path="/product/:id"
-          element={<ProductDetail />}
-        />
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </div>
   );
